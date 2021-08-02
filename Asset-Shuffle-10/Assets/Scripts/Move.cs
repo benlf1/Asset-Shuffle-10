62c67fd285136dd87c6a0bc356fc96193bbffd1d
benlf1 <54038475+benlf1@users.noreply.github.com>
2021-08-02 14:57:21 -0700

    Initial commit

---
+++ Asset-Shuffle-10/Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpForce = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1")) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Asset-Shuffle-10
+ 
